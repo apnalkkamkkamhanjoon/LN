778ba7a994e1cf23d34657d5eef93fa0b309cf7f
한준 <s22073@gsm.hs.kr>
2023-08-01 22:26:30 +0900

    add TestFile

---
+++ Test/index.js
@@ -0,0 +1,11 @@
+const { odd, even } = require("./var");
+const checkNumber = require("./func.js");
+
+function checkStringOddOrEven(str) {
+  if (str.length % 2) {
+    return odd;
+  }
+  return even;
+}
+console.log(checkNumber(10));
+console.log(checkStringOddOrEven("hello"));
